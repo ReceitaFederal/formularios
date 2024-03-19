493d9b78908e8939a123d33f21d0b9de2a56a81a
Ana Clara <anaclara.aslima@gmail.com>
2024-03-19 18:03:34 -0300

    arquivo js

--- src/html/siscaer/js/javascript.js
+++
@@ -1,9 +0,0 @@
-  function showInfo() {
-    document.getElementById('infoText').style.display = 'block';
-  }
-
-  function hideInfo() {
-    document.getElementById('infoText').style.display = 'none';
-  } 
-  
-  
---
+++ src/html/siscaer/js/script.js
@@ -0,0 +1,27 @@
+  /*function showInfo() {
+    document.getElementById('infoText').style.display = 'block';
+  }
+
+  function hideInfo() {
+    document.getElementById('infoText').style.display = 'none';
+  } */
+  
+  // o código a seguir serve para trocar as abas com o elemento tab 
+ const tabs = document.querySelectorAll('.tab-btn');
+
+ tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
+
+ const tabClicked = (tab) => {
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tab.classList.add('active');
+
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.classList.remove('show'));
+
+    const contentId = tab.getAttribute('content-id');
+    const content = document.getElementById(contentId);
+    
+    content.classList.add('show');
+ }
+ const currentActiveTab = document.querySelector('.tab-btn.active');
+ tabClicked(currentActiveTab);
