72cdf2d96bec30946a181137d19818ca8042089c
Carol <carolcardoso080@gmail.com>
2024-03-20 17:07:35 -0300

    dados_tripulacao alterado - Botão adicionar bloco

--- src/html/gendec/componentes/dados_tripulacao/dados_tripulacao.js
+++ src/html/gendec/componentes/dados_tripulacao/dados_tripulacao.js
@@ -16,13 +16,35 @@ export class Tripulacao extends HTMLElement{
                 template.innerHTML = texto_pagina;
 
                 this.appendChild(template.content.cloneNode(true));
+
+                this.adicionar_comportamento();
+
+                this.adicionar_tripulante();
                 
                 this.dispatchEvent(new CustomEvent("carregou"));                
             });
         });
 
-        
+    }
+
+    adicionar_comportamento(){
+        let adicionar = document.querySelector("#adicionar_tripulante");
+
+        adicionar.addEventListener("click", (evento) => {
+            console.log("Clicou");
+
+            this.adicionar_tripulante();
+        });
 
     }
+
+    adicionar_tripulante(){
+        let template = document.querySelector("#tripulante");
+
+        let fieldsetTripulantes = document.querySelector("#lista_tripulantes");
+
+        fieldsetTripulantes.appendChild(template.content.cloneNode(true));
+    }
+
 }
 customElements.define('br-tripulacao', Tripulacao);
