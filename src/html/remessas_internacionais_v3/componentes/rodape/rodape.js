2c6e1cf2d63dac33e159c9ff7655e7ce920228c5
Bonafé <fernando.s.fernandes@rfb.gov.br>
2024-07-27 18:53:04 -0300

    não está usando esse rodape

--- src/html/remessas_internacionais_v3/componentes/rodape/rodape.js
+++
@@ -1,9 +0,0 @@
-import { ComponenteBase } from "../../bibliotecas/ultima/componente_base.js";
-
-export class Rodape extends ComponenteBase{
-    
-    constructor(){
-        super({templateURL:"./rodape.html", shadowDOM:false}, import.meta.url);                
-    }
-}
-customElements.define('br-rodape', Rodape);
